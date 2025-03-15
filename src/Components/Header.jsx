ab09886b3a6079576f2adaab6106f351d524ad66
Bishop <addyjerry2704@gmail.com>
2025-03-15 22:21:30 +0000

    header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+import logo from '../assets/logo.png'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div id='header'>
+        <img src={logo} alt="logo" id='logo'/>
+        <Navbar/>
+        <div id='log__area'>
+            <span>Login / Registration </span> <span> |</span> <span>Book Table</span>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Contact us</li>
+            <li>Blog</li>
+            <li>Landing</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
