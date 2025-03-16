d0ad948e9d919a5b4debafe46cc4eae9c945d9ff
Bishop <addyjerry2704@gmail.com>
2025-03-16 00:40:33 +0000

    hero-section-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Components/Header'
+import { About } from './Components/About'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
    <>
    <Header/>
+   <About/>
    </>
   )
 }
---
+++ src/Components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.css'
+
+const Button = ({chidren}) => {
+  return (
+    <button>{chidren}</button>
+  )
+}
+
+export default Button
