450df00c3b728d50e67e70c5d30646625382688a
Bishop <addyjerry2704@gmail.com>
2025-03-23 20:28:22 +0000

    style update

--- src/Components/Chef/Chef.jsx
+++ src/Components/Chef/Chef.jsx
@@ -6,18 +6,20 @@ import key from "../../assets/images/spoon.png";
 const Chef = () => {
   return (
     <div className="chef">
-      <div className="chef__image">
-        <img src={chef} alt="chef" />
-      </div>
-      <div className="chef__text">
-        <h2>Jery's Word</h2>
-        <img src={key} alt="spoon" />
-        <h1>What We Believe In</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum culpa
-          animi a quod rerum vero odit veritatis harum neque? Temporibus nisi
-          voluptates cupiditate accusantium accusamus atque ea dicta exp
-        </p>
+      <div className="ontop">
+        <div className="chef__image">
+          <img src={chef} alt="chef" />
+        </div>
+        <div className="chef__text">
+          <h2>Jery's Word</h2>
+          <img src={key} alt="spoon" />
+          <h1>What We Believe In</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum culpa
+            animi a quod rerum vero odit veritatis harum neque? Temporibus nisi
+            voluptates cupiditate accusantium accusamus atque ea dicta exp
+          </p>
+        </div>
       </div>
     </div>
   );
